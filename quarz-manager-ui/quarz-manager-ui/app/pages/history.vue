19b033f69ac1bf2359e4b05dd12b342208977268
QuadStingray <git@quadstingray.dev>
2025-07-08 22:58:37 +0200

    feat: configure nuxt like https://github.com/sfxcode/nuxt3-primevue-starter

---
+++ quarz-manager-ui/quarz-manager-ui/app/pages/history.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { d, t, n, locale, locales, setLocale } = useI18n()
+
+</script>
+
+<template>
+  <div class="card surface-0 text-center">
+    <h1>{{ t('history') }}</h1>
+    <div class="gap-2 grid grid-cols-2 lg:grid-cols-3">
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
