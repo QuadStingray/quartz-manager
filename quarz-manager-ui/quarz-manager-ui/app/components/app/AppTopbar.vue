afc69f23a076377148d31e6e97285de10bd7dc4d
QuadStingray <git@quadstingray.dev>
2025-07-08 22:58:37 +0200

    feat: configure nuxt like https://github.com/sfxcode/nuxt3-primevue-starter

---
+++ quarz-manager-ui/quarz-manager-ui/app/components/app/AppTopbar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+function redirectToGithub() {
+  window.open('https://github.com/sfxcode/nuxt3-primevue-starter', '_blank')
+}
+</script>
+
+<template>
+  <nav>
+    <Toolbar>
+      <template #start >
+        Quarz Manager UI
+      </template>
+
+      <template #end>
+        <AppColorMode class="ml-6 mr-2" />
+        <Button icon="pi pi-sign-out" severity="danger" />
+      </template>
+    </Toolbar>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ quarz-manager-ui/quarz-manager-ui/app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <div class="card surface-0 text-center">
+    <div class="text-4xl font-bold mb-3">
+      <span class="text-green-600">PrimeVue </span>
+      <span class="text-blue-400"> & TypeScript</span>
+      <span class="text-green-400"> & Nuxt 3</span>
+    </div>
+    <h2 class="text-2xl pb-6">
+      Starter for Vue.js Development with Nuxt 3.
+    </h2>
+    <div class="gap-2 grid grid-cols-2 lg:grid-cols-3">
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
