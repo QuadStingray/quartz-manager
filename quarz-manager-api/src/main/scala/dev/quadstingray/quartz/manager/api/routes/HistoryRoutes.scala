0a8b7beb3764ab718dd3116cc2ea84d5224ec726
QuadStingray <git@quadstingray.dev>
2025-11-14 08:14:26 +0100

    feat: Add endpoint to fetch job history by ID

--- quarz-manager-api/src/main/scala/dev/quadstingray/quartz/manager/api/routes/HistoryRoutes.scala
+++ quarz-manager-api/src/main/scala/dev/quadstingray/quartz/manager/api/routes/HistoryRoutes.scala
@@ -1,5 +1,7 @@
 package dev.quadstingray.quartz.manager.api.routes
 import dev.quadstingray.quartz.manager.api.json.CirceSchema
+import dev.quadstingray.quartz.manager.api.model.Error
+import dev.quadstingray.quartz.manager.api.model.ErrorResponse
 import dev.quadstingray.quartz.manager.api.model.LogRecord
 import dev.quadstingray.quartz.manager.api.service.auth.AuthenticationService
 import dev.quadstingray.quartz.manager.api.service.HistoryService
@@ -10,6 +12,7 @@ import scala.concurrent.Future
 import sttp.capabilities
 import sttp.capabilities.pekko.PekkoStreams
 import sttp.model.Method
+import sttp.model.StatusCode
 import sttp.tapir._
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
@@ -35,7 +38,24 @@ class HistoryRoutes(authenticationService: AuthenticationService) extends CirceS
         }
     }
 
+  private val historyByIdEndpoint = historyApiBaseEndpoint
+    .in(path[String]("id"))
+    .out(jsonBody[LogRecord])
+    .summary("Job History by ID")
+    .description("Returns a single Job History record by its ID")
+    .method(Method.GET)
+    .name("historyById")
+    .serverLogic {
+      _ => id =>
+        Future {
+          HistoryService.cache.asMap().get(id) match {
+            case Some(logRecord) => Right(logRecord)
+            case None            => Left(Error(StatusCode.NotFound, ErrorResponse(s"History with ID $id not found")))
+          }
+        }
+    }
+
   lazy val endpoints: List[ServerEndpoint[PekkoStreams with capabilities.WebSockets, Future]] = {
-    List(historyListEndpoint)
+    List(historyListEndpoint, historyByIdEndpoint)
   }
 }
