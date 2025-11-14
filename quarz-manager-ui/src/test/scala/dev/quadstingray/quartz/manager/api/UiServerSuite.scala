a27d52495d7bf40201ad91ccc5a03d5b1a28c5d1
QuadStingray <git@quadstingray.dev>
2025-11-14 07:19:36 +0100

    refactor: style and project structure

--- quarz-manager-ui/src/test/scala/dev/quadstingray/quartz/manager/api/UiServerSuite.scala
+++ quarz-manager-ui/src/test/scala/dev/quadstingray/quartz/manager/api/UiServerSuite.scala
@@ -1,18 +1,14 @@
 package dev.quadstingray.quartz.manager.api
+import dev.quadstingray.quartz.manager.api.model.JobConfig
+import dev.quadstingray.quartz.manager.api.service.ClassGraphService
+import dev.quadstingray.quartz.manager.api.service.JobSchedulerService
 import dev.quadstingray.quartz.manager.ui.QuarzManagerUi
-import scala.concurrent.Future
-import sttp.capabilities.pekko.PekkoStreams
-import sttp.capabilities.WebSockets
-import sttp.tapir.emptyInput
-import sttp.tapir.files.staticResourcesGetServerEndpoint
-import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.stringToPath
+import org.quartz.impl.StdSchedulerFactory
 
 class UiServerSuite extends munit.FunSuite {
   test("list users as admin") {
-    val server = new Server()
-    server.registerAfterLoadedRoutes(QuarzManagerUi.uiRoutes)
-    server.startServer()
-    while (true) {}
+
+//    while (true) {}
+
   }
 }
